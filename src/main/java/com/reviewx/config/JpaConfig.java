6fcbd88373ad4d7b795dfd713497c6b2804ebfb8
VX Platform <vx@reviewx.com>
2025-10-13 13:52:55 +0900

    Reset repository

--- src/main/java/com/reviewx/config/JpaConfig.java
+++
@@ -1,8 +0,0 @@
-package com.reviewx.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class JpaConfig {
-    // JPA Auditing은 ReviewXApplication에서 설정됨
-}
